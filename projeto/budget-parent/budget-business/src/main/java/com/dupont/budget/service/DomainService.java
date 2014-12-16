0d4588936bd51fb7270ceb89580fbec3392716ed
Joel Santos <joel.santos@surittec.com.br>
2014-12-16 17:31:31 -0300

    Retirada de métodos desnecessários.

--- projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/DomainService.java
+++ projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/DomainService.java
@@ -2,9 +2,7 @@ package com.dupont.budget.service;
 
 import java.util.List;
 
-import com.dupont.budget.exception.DuplicateEntityException;
 import com.dupont.budget.model.AbstractEntity;
-import com.dupont.budget.model.Cultura;
 import com.dupont.budget.model.NamedAbstractEntity;
 import com.dupont.budget.model.PapelUsuario;
 import com.dupont.budget.model.Usuario;
@@ -18,40 +16,6 @@ import com.dupont.budget.model.Usuario;
  */
 public interface DomainService {
 
-	/**
-	 * Cria a entidade cultura no meio persistente
-	 * @param cultura entidade a ser criada
-	 * @return entidade criada
-	 * @throws DuplicateEntityException se uma entidade com mesmo nome ja existe
-	 */
-	public Cultura createCultura(Cultura cultura) throws DuplicateEntityException;
-	
-	/**
-	 * Retorna todas as entidades culturas do meio persistente
-	 * @return lista com todas as entidades criadas
-	 */
-	public List<Cultura> findAllCulturas();
-	
-	/**
-	 * Retorna todas as entidades culturas que atendam o filtrol
-	 * @param nome String inicial do nome da cultura.
-	 * @return lista com as culturas encontradas.
-	 */
-	public List<Cultura> findCulturasByNome(String nome);
-	
-	/**
-	 * Remove a cultura do meio persistente.
-	 * @param cultura entidade cultura.
-	 */
-	public void deleteCultura(Cultura cultura);
-	
-	/**
-	 * Atualiza a entidade cultura.
-	 * @param cultura entidade a ser atualizada.
-	 * @return entidade atualizada
-	 */
-	public Cultura updateCultura(Cultura cultura);
-	
 	/**
 	 * Retorna um usuario pelo login.
 	 * @param login login do usuario
--- projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/impl/DomainServiceBean.java
+++ projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/impl/DomainServiceBean.java
@@ -6,9 +6,6 @@ import javax.ejb.Stateless;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.dupont.budget.exception.DuplicateEntityException;
-import com.dupont.budget.model.CentroCusto;
-import com.dupont.budget.model.Cultura;
 import com.dupont.budget.model.PapelUsuario;
 import com.dupont.budget.model.Usuario;
 import com.dupont.budget.service.DomainService;
@@ -25,72 +22,21 @@ import com.dupont.budget.service.GenericService;
 public class DomainServiceBean extends GenericService implements DomainService {
 
 	@Override
-	public Cultura createCultura(Cultura cultura) throws DuplicateEntityException {
-		
-		// Verifica se existe uma cultura com o mesmo nome. Caso exista, lançar uma exceção.
-		List<Cultura> exists =  em.createNamedQuery("Cultura.findByName", Cultura.class)
-									.setParameter("nome", cultura.getNome().trim())
-									.getResultList();
-		
-		if( ! exists.isEmpty() )
-			throw new DuplicateEntityException(cultura);
-				
-		em.persist(cultura);
-		
-		return cultura;
-	}
-
-	@Override
-	public List<Cultura> findAllCulturas() {		
-		
-		return em.createNamedQuery("Cultura.findAll", Cultura.class).getResultList();
-	}
-
-	@Override
-	public List<Cultura> findCulturasByNome(String nome) {
-		
-		// Se não preencheu o nome, retornar todas as entidades
-		if( StringUtils.isBlank(nome) )
-			return findAllCulturas();
-
-		List<Cultura> result =  em.createNamedQuery("Cultura.findByName", Cultura.class)
-									.setParameter("nome", nome.trim() + "%")
-									.getResultList();
-		
-		return result;
-	}
-
-	@Override
-	public void deleteCultura(Cultura cultura) {	
-		cultura = em.merge(cultura); // attaching the entity again
-		em.remove(cultura);
-	}
-
-	@Override
-	public Cultura updateCultura(Cultura cultura) {
-
-		return em.merge(cultura);
-	}
-
-	@Override
 	public Usuario getUsuarioByLogin(String login) {
 		
 		if( StringUtils.isBlank(login) )
 			return null;
 		
-		
 		Usuario result = em.createNamedQuery("Usuario.findByLogin", Usuario.class)
 							.setParameter("login", login)
 							.getSingleResult();
-		
 		return result;
 	}
 
 	@Override
 	public List<PapelUsuario> findPapeisByCentroDeCusto(Long idCentroDeCusto) {
 		
-		return 
-				 em.createNamedQuery("PapelUsuario.findByCentroDeCusto", PapelUsuario.class)
+		return em.createNamedQuery("PapelUsuario.findByCentroDeCusto", PapelUsuario.class)
 					.setParameter("idCentroDeCusto", idCentroDeCusto)
 					.getResultList();
 	}
