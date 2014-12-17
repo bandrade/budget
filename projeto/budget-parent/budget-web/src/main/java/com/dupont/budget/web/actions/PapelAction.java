a103ddecbfd913b71d4f39881c80ab47371a3391
Joel Santos <joel.santos@surittec.com.br>
2014-12-17 10:27:26 -0300

    Criação do cadastro de papel e correção na consulta pelo nome.

---
+++ projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/actions/PapelAction.java
@@ -0,0 +1,64 @@
+package com.dupont.budget.web.actions;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+
+import com.dupont.budget.model.Papel;
+import com.dupont.budget.service.DomainService;
+import com.dupont.budget.web.util.FacesUtils;
+/**
+ * Controller das telas de manutenção da entidade produto
+ * 
+ * @author <a href="bandrade@redhat.com">Bruno Andrade</a>
+ * @since 2014
+ *
+ */
+@Model
+@RolesAllowed(value = "ADMINISTRADOR")
+public class PapelAction extends GenericAction<Papel> {
+
+	private static final long serialVersionUID = -9064126463852854590L;
+
+	@Inject
+	private DomainService service;
+
+	@Inject
+	private Logger logger;
+
+	@Inject
+	private FacesUtils facesUtils;
+
+	@Named
+	@Produces
+	public Papel getPapel() {
+		return getEntidade();
+	}
+
+	/**
+	 * Buscar as culturas a partir do filtro.
+	 */
+	public void find() {
+		list = service.findByName(entidade);
+	}
+
+	@Override
+	protected Logger getLogger() {
+		return logger;
+	}
+
+	@Override
+	protected DomainService getService() {
+		return service;
+	}
+
+	@Override
+	protected FacesUtils getFacesUtils() {
+		return facesUtils;
+	}
+}
+	
