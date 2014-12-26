ba6b3f03ebee56674c59384c7a05fd841674ea79
Joel Santos <joel.santos@surittec.com.br>
2014-12-26 11:05:41 -0300

    Adição de filtro para correção de coding.

---
+++ projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/filters/CharacterEncodingFilter.java
@@ -0,0 +1,78 @@
+package com.dupont.budget.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Verifica se a requisição atual possui o encoding definido e configura, caso
+ * tenha sido especificado nos parametros de inicizlização do filtro o enconding
+ * esperado.
+ * 
+ * @author joel
+ *
+ */
+@WebFilter(initParams = @WebInitParam(name = "encoding", value = "UTF-8"), urlPatterns = "*.xhtml")
+public class CharacterEncodingFilter implements Filter {
+	protected String encoding = null;
+	protected FilterConfig filterConfig = null;
+	protected boolean ignore = true;
+
+	/**
+	 * Take this filter out of service.
+	 */
+	public void destroy() {
+		this.encoding = null;
+		this.filterConfig = null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+	 * javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		if (ignore || (request.getCharacterEncoding() == null)) {
+			String encoding = selectEncoding(request);
+			if (encoding != null)
+				request.setCharacterEncoding(encoding);
+		}
+
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+		this.encoding = filterConfig.getInitParameter("encoding");
+		String value = filterConfig.getInitParameter("ignore");
+		if (value == null)
+			this.ignore = true;
+		else if (value.equalsIgnoreCase("true"))
+			this.ignore = true;
+		else if (value.equalsIgnoreCase("yes"))
+			this.ignore = true;
+		else
+			this.ignore = false;
+	}
+
+	protected String selectEncoding(ServletRequest request) {
+		return (this.encoding);
+	}
+}
