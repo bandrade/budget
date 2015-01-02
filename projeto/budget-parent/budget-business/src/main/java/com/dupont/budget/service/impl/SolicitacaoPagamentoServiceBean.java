fa044361a8dd93379f0ce4839d9cc35fd739d461
bandrade <bandrade@localhost.localdomain>
2015-01-02 17:30:52 -0200

    correcao

--- projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/impl/SolicitacaoPagamentoServiceBean.java
+++ projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/impl/SolicitacaoPagamentoServiceBean.java
@@ -11,33 +11,33 @@ import com.dupont.budget.service.bpms.BPMSProcessService;
 
 /**
  * Implementacao do servico que controla a solicitação de pagamento.
- * 
+ *
  * @author <a href="asouza@redhat.com">Ângelo Galvão</a>
  * @since 2014
  *
  */
 @Stateless
 public class SolicitacaoPagamentoServiceBean implements SolicitacaoPagamentoService {
-	
+
 	@Inject
 	private BPMSProcessService processService;
-	
+
 	@PersistenceContext(unitName="budget-pu")
 	protected EntityManager em;
 
 	@Override
 	public void startSolicitacaoPagamento( SolicitacaoPagamento solicitacaoPagamento) {
-		
+
 
 		// Inicia o processo
 		long processInstanceId = 0;
 		try {
-			processInstanceId = processService.iniciarProcessoSolicitacaoPagamento(null, null, null);
+			processInstanceId = processService.iniciarProcessoSolicitacaoPagamento(null);
 		} catch (Exception e) {
 		}
-		
+
 		solicitacaoPagamento.setProcessInstanceId(processInstanceId);
-		
+
 		// salva no banco
 		em.persist(solicitacaoPagamento);
 
