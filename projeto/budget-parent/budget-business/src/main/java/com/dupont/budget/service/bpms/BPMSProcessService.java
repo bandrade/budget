52b76858877f55e6a588abfea797ad5b702ab235
bandrade <bandrade@localhost.localdomain>
2015-01-02 16:43:20 -0200

    alteracao solicitacao de pagamento

--- projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/bpms/BPMSProcessService.java
+++ projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/bpms/BPMSProcessService.java
@@ -1,11 +1,12 @@
 package com.dupont.budget.service.bpms;
 
+import com.dupont.budget.dto.SolicitacaoPagamentoDTO;
 import com.dupont.budget.model.Area;
 
 
 public interface BPMSProcessService {
 	long iniciarProcessoBudget(String ano) throws Exception;
-	long iniciarProcessoSolicitacaoPagamento(Area area, Long idSolicitacao,String numeroNota) throws Exception;
+	long iniciarProcessoSolicitacaoPagamento(SolicitacaoPagamentoDTO [] solicitacoes) throws Exception;
 	Object obterVariavelProcesso(Long idProcesso,String variavel) throws Exception;
 	boolean existeProcessoAtivo(String ano) throws Exception;
 
