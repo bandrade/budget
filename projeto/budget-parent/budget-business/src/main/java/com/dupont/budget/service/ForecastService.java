bcbe0ca20c00a5fb2a682587112993f868a0d56c
Ângelo Galvão <asouza@redhat.com>
2015-01-05 11:06:05 -0200

    Adiconar suporte a forecast no solicitaçao de pagamentos

--- projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/ForecastService.java
+++ projeto/budget-parent/budget-business/src/main/java/com/dupont/budget/service/ForecastService.java
@@ -1,6 +1,12 @@
 package com.dupont.budget.service;
 
+import java.util.List;
+
+import com.dupont.budget.model.Forecast;
+
 public interface ForecastService {
 
 	void criarPrimeiroForecast(String budgetId);
+	
+	List<Forecast> findForecastsByBudgetId(Long id);
 }
