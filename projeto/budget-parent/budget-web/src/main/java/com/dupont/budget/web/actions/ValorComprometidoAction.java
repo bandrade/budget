96f440508b192603b68b791fe2ed7773a4f6e851
Ângelo Galvão <asouza@redhat.com>
2015-01-09 11:14:24 -0200

    Defeito #26

--- projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/actions/ValorComprometidoAction.java
+++ projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/actions/ValorComprometidoAction.java
@@ -3,11 +3,11 @@ package com.dupont.budget.web.actions;
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
-import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.deltaspike.core.api.scope.ViewAccessScoped;
 import org.slf4j.Logger;
 
 import com.dupont.budget.model.Acao;
@@ -25,7 +25,7 @@ import com.dupont.budget.web.util.FacesUtils;
  * @since 2014
  *
  */
-@Model
+@ViewAccessScoped @Named
 @RolesAllowed(value = "ADMINISTRADOR")
 public class ValorComprometidoAction extends AsyncFileUploadAction<ValorComprometido> {
 
