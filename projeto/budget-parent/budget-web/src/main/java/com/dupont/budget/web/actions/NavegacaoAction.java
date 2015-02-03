91007299456099d3d469a6fe8130378185e79c77
bandrade <bandrade@localhost.localdomain>
2015-02-03 11:07:37 -0200

    ajuste por centro de custo

--- projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/actions/NavegacaoAction.java
+++ projeto/budget-parent/budget-web/src/main/java/com/dupont/budget/web/actions/NavegacaoAction.java
@@ -18,7 +18,8 @@ public class NavegacaoAction {
 		navegationMap.put("Aprovar Budget", "aprovarBudget");
 		navegationMap.put("Auditar Budget", "auditarBudget");
 		navegationMap.put("Inserir Budget Aprovado", "inserirBudgetAprovado");
-		navegationMap.put("Ajustar o Budget aprovado por Centro de Custo", "ajustarValoresCC");
+		navegationMap.put("Ajustar o Budget aprovado por CC", "ajustarValoresCC");
+		navegationMap.put("Ajustar o Budget aprovado por Area", "ajustarValoresArea");
 		navegationMap.put("Dividir budget por mes", "divisaoBudgetMes");
 		navegationMap.put("Efetuar Entrada da Nota", "detalharPagamentoDespesa");
 		navegationMap.put("Atualizar Forecast", "atualizarForecast");
